ea16a980a0988c746ef2319cff413dac8e4d8cb3
MORI Kazutaka@Zenbook <mori.kazutaka@gmail.com>
2021-01-14 11:14:15 +0900

    読み込みに問題が出ていたパッケージのための修正
    RSPMの "INSTALL SYSTEM PREREQUISITES FOR UBUNTU 20.04 (FOCAL)" を参照

--- my_scripts/install_tinytex.sh
+++ my_scripts/install_tinytex.sh
@@ -60,3 +60,7 @@ Rscript -e 'tinytex::install_tinytex(dir = "/home/rstudio/.TinyTeX/")'
     zhnumber
 
 /home/rstudio/.TinyTeX/bin/x86_64-linux/tlmgr path add
+
+# {tikzDevice} package 用にLaTeXコンパイラの場所を指定
+echo "R_LATEXCMD=/home/rstudio/.TinyTeX/bin/x86_64-linux/platex" >> /home/rstudio/.Renviron
+echo "R_PDFLATEXCMD=/home/rstudio/.TinyTeX/bin/x86_64-linux/pdflatex" >> /home/rstudio/.Renviron
