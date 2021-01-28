7be09d02a8fceca782a2943304328dbfc3cc8ac9
MORI Kazutaka@Zenbook <mori.kazutaka@gmail.com>
2021-01-29 00:43:18 +0900

    install_tinytex.sh を分割

--- my_scripts/install_tinytex.sh
+++ my_scripts/install_tinytex.sh
@@ -1,65 +1,8 @@
 #!/bin/bash
 
-# TinyTex本体と必要なパッケージのインストール
-
-set -x
+# TinyTex本体のインストール
 
 Rscript -e 'tinytex::install_tinytex(dir = "/home/rstudio/.TinyTeX/")'
-
-/home/rstudio/.TinyTeX/bin/x86_64-linux/tlmgr install \
-    bxjscls \
-    zxjatype \
-    zxjafont \
-    adobemapping \
-    amscls \
-    arphic \
-    beamer \
-    cjk \
-    cjkpunct \
-    cns \
-    ctablestack \
-    ctex \
-    everyhook \
-    fandol \
-    fonts-tlwg \
-    fp \
-    garuda-c90 \
-    latex-base-dev \
-    luatexbase \
-    luatexja \
-    mptopdf.x86_64-linux \
-    mptopdf \
-    ms \
-    norasi-c90 \
-    pgf \
-    platex.x86_64-linux \
-    platex \
-    platex-tools \
-    ptex.x86_64-linux \
-    ptex \
-    ptex-base \
-    ptex-fonts \
-    svn-prov \
-    translator \
-    ttfutils.x86_64-linux \
-    ttfutils \
-    uhc \
-    ulem \
-    uplatex.x86_64-linux \
-    uplatex \
-    uptex.x86_64-linux \
-    uptex \
-    uptex-base \
-    uptex-fonts \
-    wadalab \
-    xcjk2uni \
-    xecjk \
-    xpinyin \
-    zhmetrics \
-    zhmetrics-uptex \
-    zhnumber
-
-/home/rstudio/.TinyTeX/bin/x86_64-linux/tlmgr path add
 
 # {tikzDevice} package 用にLaTeXコンパイラの場所を指定
 echo "R_LATEXCMD=/home/rstudio/.TinyTeX/bin/x86_64-linux/platex" >> /home/rstudio/.Renviron
