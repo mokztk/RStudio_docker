9853d09421ef0d49cc0adea827bdca9202933c22
MORI Kazutaka@Zenbook <mori.kazutaka@gmail.com>
2021-02-01 10:42:38 +0900

    TinyTeXインストール時に ~ にできる bin/ を削除

--- my_scripts/install_tinytex.sh
+++ my_scripts/install_tinytex.sh
@@ -4,6 +4,11 @@
 
 Rscript -e 'tinytex::install_tinytex(dir = "/home/rstudio/.TinyTeX/")'
 
+# /home/rstudio に bin/ が出来てしまうので消しておく
+if [ -e '/home/rstudio/bin' ]; then
+    rm -rf /home/rstudio/bin
+fi
+
 # {tikzDevice} package 用にLaTeXコンパイラの場所を指定
 echo "R_LATEXCMD=/home/rstudio/.TinyTeX/bin/x86_64-linux/platex" >> /home/rstudio/.Renviron
 echo "R_PDFLATEXCMD=/home/rstudio/.TinyTeX/bin/x86_64-linux/pdflatex" >> /home/rstudio/.Renviron
