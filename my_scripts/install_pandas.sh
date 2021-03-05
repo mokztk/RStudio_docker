79d5ac83db7a0dac6bccf1a687d78241d62bad2a
MORI Kazutaka@Zenbook <mori.kazutaka@gmail.com>
2021-03-06 01:11:36 +0900

    pythonを /rocker_scripts/install_python.sh でインストールするようにした
    radianのインストールで、現時点では jedi 0.18 以降に対応しないので公式の説明に従いバージョンを固定するようにした
    
    (cherry picked from commit aa16cd983800a9b3fe8c8696264cfef056f0984e)

--- my_scripts/install_pandas.sh
+++ my_scripts/install_pandas.sh
@@ -5,14 +5,11 @@ set -x
 # R から {reticulate} で Python3 を使えるようにセットアップ
 
 # Python3のインストール
-apt-get update
-apt-get install -y libpython3.8 python3-pandas python3-pip
+source /rocker_scripts/install_python.sh
+
+# aptキャッシュを消去
 apt-get clean
 rm -rf /var/lib/apt/lists/*
 
-# python の共有ライブラリを登録
-echo "/usr/lib/x86_64-linux-gnu" > /etc/ld.so.conf.d/libpython3.8.conf
-ldconfig
-
-# R に {reticulate} をインストール
-R -e 'install.packages("reticulate")'
+# グローバルに pandas を入れておく
+pip install pandas
