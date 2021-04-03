22f8e75f845d532fc8497ed26b7a4bf26ca40654
MORI Kazutaka@Zenbook <mori.kazutaka@gmail.com>
2021-04-04 01:31:11 +0900

    TinyTeX の repo を TeX Live 2020 frozen に変更
    日本語 TeX 開発コミュニティ texjp.org のサーバにあるTeX Live 2020 のアーカイブを利用
    TinyTeX は TeX Live 2020 の freeze より前の "2021.03" を利用

--- my_scripts/install_tinytex.sh
+++ my_scripts/install_tinytex.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # TinyTex本体のインストール
+# 当面、FREEZEとなった TeXLive 2020 アーカイブを利用する
 
-Rscript -e 'tinytex::install_tinytex(dir = "/home/rstudio/.TinyTeX/")'
+Rscript -e 'tinytex::install_tinytex(dir = "/home/rstudio/.TinyTeX/", version = "2021.03", repository = "https://texlive.texjp.org/2020/tlnet")'
 
 # 一部の package 用にLaTeXコンパイラの場所を指定
 echo "R_LATEXCMD=/home/rstudio/.TinyTeX/bin/x86_64-linux/platex" >> /home/rstudio/.Renviron
