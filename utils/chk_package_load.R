cc50a702f3311efab43cb448580cf84eaaeb053e
MORI Kazutaka@desktop <mori.kazutaka@gmail.com>
2021-04-06 17:21:04 +0900

    chk_package_load.R の内容を強化
    - Warning, Error の内容を返すようにした
    - library() で読み込むたびに初期状態から増えた読み込み済みパッケージを外し、どのパッケージも初期状態から library() をしたのと同じ状態にした
    - 結果をCSVに保存するようにした

--- utils/chk_package_load.R
+++ utils/chk_package_load.R
@@ -1,27 +1,47 @@
 # 読み込めないパッケージを洗い出す
 
+# 開始時に読み込まれているパッケージのリスト
+base_list <- search()
+
+# インストールされている全パッケージを一つずつ試す
 res <- sapply(
-  # インストールされている全パッケージ
   .packages(all.available = TRUE),
   function(pkg){
     temp <- tryCatch(
         # 極力メッセージが表示されないようにパッケージを読み込む
         suppressMessages(
-          require(pkg, character.only = TRUE, quietly = TRUE)
+          library(pkg, character.only = TRUE, quietly = TRUE)
         ),
-        # バイナリパッケージビルド時のRのバージョン違いは"ok"とする
+        # Warning, Error の場合はその内容を返す。改行はスペースに置換
         warning = function(w){
-          ifelse(regexpr("was\ built\ under\ R\ version", w$message) > 0,
-                 "ok", "Warning")
+          return(paste("[Warning]", gsub("\n", " ", w$message)))
         },
-        # エラーはとりあえずそのまま "Error" で返す
-        error = function(e){ return("Error") }
-      )
+        error = function(e){
+          return(paste("[Error]", gsub("\n", " ", e$message)))
+        }
+    )
+    
+    # 初期状態と比較して、新しくロードされたものを detach() する
+    sapply(setdiff(search(), base_list),
+           function(p) detach(pos = match(p, search())))
     
     # パッケージのロード成功の場合は "ok" で返す
-    temp <- ifelse(temp == "TRUE", "ok", temp)
+    temp <- ifelse(pkg %in% temp, "ok", temp)
   })
 
+# data frame に変換
+res_df <- data.frame(cbind(
+  package = names(res),
+  result  = unname(res)
+))
+
+# 文字化け対策
+res_df$result <- gsub("‘", "'", res_df$result)
+res_df$result <- gsub("’", "'", res_df$result)
+
+# CSVに結果を保存
+write.csv(res_df, file = "~/library_test.csv")
+
 # 読み込みに失敗したもの
-res[res == "Warning"]
-res[res == "Error"]
+res_df[grepl("\\[Warning\\]", res_df$result),]
+res_df[grepl("\\[Error\\]", res_df$result),]
