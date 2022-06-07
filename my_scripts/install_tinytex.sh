675297a2754d2f86d6dacb92ead30ae112459a0f
MORI Kazutaka <mori.kazutaka@gmail.com>
2022-06-07 16:15:48 +0900

    TinyTeXインストール時に原ノ味フォント（TeXLive標準）を入れておく。ほかは必要時自動で入る

--- my_scripts/install_tinytex.sh
+++ my_scripts/install_tinytex.sh
@@ -5,6 +5,9 @@
 
 Rscript -e 'tinytex::install_tinytex(dir = "/home/rstudio/.TinyTeX/", version = "2022.03", repository = "https://texlive.texjp.org/2021/tlnet")'
 
+# 原ノ味フォントをインストールしておく（その他は、必要時に tlmgr で自動的にインストールされる）
+Rscript -e 'tinytex::tlmgr_install("haranoaji")'
+
 # 一部の package 用にLaTeXコンパイラの場所を指定
 echo "R_LATEXCMD=/home/rstudio/.TinyTeX/bin/x86_64-linux/platex" >> /home/rstudio/.Renviron
 echo "R_PDFLATEXCMD=/home/rstudio/.TinyTeX/bin/x86_64-linux/pdflatex" >> /home/rstudio/.Renviron
