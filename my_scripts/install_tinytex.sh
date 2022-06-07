18868752b908f48fc8a1bd60df5eccc2dc8eef31
MORI Kazutaka <mori.kazutaka@gmail.com>
2022-06-07 09:16:18 +0900

    TinyTeXインストール時に指定するレポジトリを日本語TeX開発コミュニティのTL2021 Frozenに変更

--- my_scripts/install_tinytex.sh
+++ my_scripts/install_tinytex.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # TinyTex本体のインストール
-# 当面、FREEZEとなった TeXLive 2020 アーカイブを利用する
+# 当面、FREEZEとなった TeXLive 2021 アーカイブを利用する
 
-Rscript -e 'tinytex::install_tinytex(dir = "/home/rstudio/.TinyTeX/", version = "2021.03", repository = "https://texlive.texjp.org/2020/tlnet")'
+Rscript -e 'tinytex::install_tinytex(dir = "/home/rstudio/.TinyTeX/", version = "2022.03", repository = "https://texlive.texjp.org/2021/tlnet")'
 
 # 一部の package 用にLaTeXコンパイラの場所を指定
 echo "R_LATEXCMD=/home/rstudio/.TinyTeX/bin/x86_64-linux/platex" >> /home/rstudio/.Renviron
