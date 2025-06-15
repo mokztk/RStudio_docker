02ea3ec024c522c15e31886f9fbe055328709c57
MORI Kazutaka <mori.kazutaka@gmail.com>
2025-06-16 01:32:41 +0900

    pakのメタデータを削除（70MB程度節約）

--- my_scripts/install_r_packages_pak.sh
+++ my_scripts/install_r_packages_pak.sh
@@ -66,12 +66,10 @@ mkdir -p /home/rstudio/.cache/R/R.cache
 chown -R rstudio:rstudio /home/rstudio/.cache
 
 # Clean up
-# Ref: https://github.com/rocker-org/rocker-versioned2/commit/75dd95c6cee7da29ceed363b9fe4823a12f575f8
+Rscript -e "pak::meta_clean(force = TRUE)"
 rm -rf /tmp/downloaded_packages
 
 apt-get clean
 rm -rf /var/lib/apt/lists/*
 
-## Strip binary installed lybraries from RSPM
-## https://github.com/rocker-org/rocker-versioned2/issues/340
 strip /usr/local/lib/R/site-library/*/libs/*.so
